5d8cfef1680d81db7c11f2d0a612b6668007a88c
Mitch Downey <mdowney@opinionlab.com>
2015-09-07 20:42:37 -0500

    Add Local Notifications demo

--- AlertsiOS/AlertsiOS/AppDelegate.swift
+++ AlertsiOS/AlertsiOS/AppDelegate.swift
@@ -16,9 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-//        UINavigationBar.appearance().barStyle = UIBarStyle.Black
+        // Alert the user to enable background notifications
+        let registerUserNotificationSettings = UIApplication.instancesRespondToSelector("registerUserNotificationSettings:")
+        if registerUserNotificationSettings {
+            var types: UIUserNotificationType = UIUserNotificationType.Alert | UIUserNotificationType.Sound
+            UIApplication.sharedApplication().registerUserNotificationSettings(UIUserNotificationSettings(forTypes: types, categories: nil))
+        }
         
         return true
+        
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -29,6 +35,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        var localNotification = UILocalNotification()
+        localNotification.alertAction = "Testing notifications"
+        localNotification.alertBody = "You have 5 new unread alerts"
+        localNotification.fireDate = NSDate(timeIntervalSinceNow: 3)
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+        
+        localNotification.alertAction = "Testing notifications"
+        localNotification.alertBody = "You have 1 new *urgent* alert"
+        localNotification.fireDate = NSDate(timeIntervalSinceNow: 3)
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+        
+        UIApplication.sharedApplication().applicationIconBadgeNumber = 2
+        
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
